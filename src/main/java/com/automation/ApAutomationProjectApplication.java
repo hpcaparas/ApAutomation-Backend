24ce6543f14d07ad3bf5ffd7810ebb69d804ebea
unknown <hpcaparas@gmail.com>
2023-11-29 02:58:57 -0500

    initial

---
+++ src/main/java/com/automation/ApAutomationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApAutomationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApAutomationProjectApplication.class, args);
+	}
+
+}
